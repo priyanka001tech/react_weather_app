b85450d16052a99a37cac2964cad180920be8fe4
priyanka001tech <priyanakothari751@gmail.com>
2021-08-01 14:03:47 +0530

    additional changes

--- src/App.js
+++ src/App.js
@@ -82,7 +82,7 @@ const API_key="d959064328a4d86ca33ad1d620d90e17";
 
 
     if(city && country){
-      const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_key}`);
+      const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_key}`);
 
       const response = await api_call.json();
       console.log(response);
